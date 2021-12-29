91d174a77ea33b60c2376864d548204881e06c48
gnstwlk <92362871+gnstwlk@users.noreply.github.com>
2021-12-29 22:16:24 +0300

    Delete MathLibrary directory

--- MathLibrary/MathLibrary/MathLibrary.cpp
+++
@@ -1,9 +0,0 @@
-// MathLibrary.cpp : Defines the exported functions for the DLL.
-#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
-#include "MathLibrary.h"
-
-
-void ATTRIBUTE(LPSTR a)
-{
-	SetFileAttributesA(a, FILE_ATTRIBUTE_READONLY);
-}
